3c5bcccd728624a95a049b082671cc0a256cd1c8
KDY <eodud1992@gmail.com>
2024-07-09 18:23:13 +0900

    시발

---
+++ test1_240709/src/test/ex/hell_world.java
@@ -0,0 +1,15 @@
+package test.ex;
+
+public class hell_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		name();
+	}
+
+	public static void name()
+	{
+		System.out.println("dsfdsafds");
+//		System.out.printf("%d / %d = %d", 100, 200, 0.5);
+	}
+}
---
+++ test1_240709/src/test/ex1_240709/test2.java
@@ -0,0 +1,25 @@
+package test.ex1_240709;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//변수 선언 및 데이터 입력시 자료형에 맞는 값 지정
+		int a = 10;	//정수형 데이터를 저장할 공간 a에 10을 저장한다
+		int b;
+		b = 20;
+		
+		int c, d; // 변수 두 개 이상을 동시에 선언함
+		c = a + b;
+		d = a - b;
+		
+		System.out.println();
+		System.out.printf("%d %d %d %d \n", a, b, c, d);
+		System.out.format("%d, %d, %d, %d\n", a, b, c, d);
+		
+		//java는 다른 자료형끼리 연산 됨
+		
+	}
+
+}
