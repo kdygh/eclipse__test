e0dd0054e0176e50100fccc723d0e33f55c4e9ed
KDY <eodud1992@gmail.com>
2024-07-12 18:20:20 +0900

    dsfdsf

--- src/test_240712/RPS.java
+++ src/test_240712/RPS.java
@@ -15,6 +15,65 @@ public class RPS {
 		
 		System.out.print("미 : ");
 		String mi = br.readLine();
+		
+		String winner; 
+		
+		switch(jun)
+		{
+		case "가위":
+		{
+			if(mi.equals("바위"))
+			{
+				winner = "미";
+			}
+			else if(mi.equals("보"))
+			{
+				winner = "준";
+			}
+			else if(mi.equals("가위"))
+			{
+				winner = "없음";
+			}
+		}
+		break;
+		case "바위":
+		{
+			if(mi.equals("가위"))
+			{
+				winner = "준";
+			}
+			else if(mi.equals("바위"))
+			{
+				winner = "비김";
+			}
+			else if(mi.equals("보"))
+			{
+				winner = "미";
+			}
+		}
+		break;
+		case "보":
+		{
+			if(mi.equals("가위"))
+			{
+				winner = "미";
+			}
+			else if(mi.equals("바위"))
+			{
+				winner = "준";
+			}
+			else if(mi.equals("보"))
+			{
+				winner = "비김";
+			}
+		}
+		break;
+		default:
+		{
+			System.out.println("무효");
+			System.exit(0);
+		}
+			break;
+		}
 	}
-
 }
