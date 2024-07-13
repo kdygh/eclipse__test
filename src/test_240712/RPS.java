bac280c50cd51db849cb6d78b0f06b673aa4046c
kdygh <eodud1992@gmail.com>
2024-07-13 15:32:48 +0900

    while

--- src/test_240712/RPS.java
+++ src/test_240712/RPS.java
@@ -10,72 +10,75 @@ public class RPS {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		System.out.print("준 : ");
-		String jun = br.readLine();
-		
-		System.out.print("미 : ");
-		String mi = br.readLine();
-		
-		String winner = null; 
-		
-		switch(jun)
+		while(true)
 		{
-		case "가위":
-		{
-			if(mi.equals("바위"))
+			System.out.print("준 : ");
+			String jun = br.readLine();
+			
+			System.out.print("미 : ");
+			String mi = br.readLine();
+			
+			String winner = null; 
+			
+			switch(jun)
 			{
-				winner = "미";
-			}
-			else if(mi.equals("보"))
+			case "가위":
 			{
-				winner = "준";
+				if(mi.equals("바위"))
+				{
+					winner = "미";
+				}
+				else if(mi.equals("보"))
+				{
+					winner = "준";
+				}
+				else if(mi.equals("가위"))
+				{
+					winner = "비김";
+				}
 			}
-			else if(mi.equals("가위"))
-			{
-				winner = "비김";
-			}
-		}
-		break;
-		case "바위":
-		{
-			if(mi.equals("가위"))
-			{
-				winner = "준";
-			}
-			else if(mi.equals("바위"))
-			{
-				winner = "비김";
-			}
-			else if(mi.equals("보"))
-			{
-				winner = "미";
-			}
-		}
-		break;
-		case "보":
-		{
-			if(mi.equals("가위"))
-			{
-				winner = "미";
-			}
-			else if(mi.equals("바위"))
-			{
-				winner = "준";
-			}
-			else if(mi.equals("보"))
-			{
-				winner = "비김";
-			}
-		}
-		break;
-		default:
-		{
-			System.out.println("무효");
-			System.exit(0);
-		}
 			break;
+			case "바위":
+			{
+				if(mi.equals("가위"))
+				{
+					winner = "준";
+				}
+				else if(mi.equals("바위"))
+				{
+					winner = "비김";
+				}
+				else if(mi.equals("보"))
+				{
+					winner = "미";
+				}
+			}
+			break;
+			case "보":
+			{
+				if(mi.equals("가위"))
+				{
+					winner = "미";
+				}
+				else if(mi.equals("바위"))
+				{
+					winner = "준";
+				}
+				else if(mi.equals("보"))
+				{
+					winner = "비김";
+				}
+			}
+			break;
+			default:
+			{
+				System.out.println("무효");
+				System.exit(0);
+			}
+				break;
+			}
+			
+			System.out.println("승자 : " + winner);
 		}
-		
-		System.out.println("승자 : " + winner);
 	}
 }
